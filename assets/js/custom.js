fa2c090a8bc7ef2b36ab3fc5253dc96647bab94b
rsaiDt7Solutions <rsaikumar201@gmail.com>
2023-04-25 15:24:33 +0530

    contact form completed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -254,3 +254,176 @@ $('.services-item').hover(function () {
   
 
 })(window.jQuery);
+
+
+function sendingmail() {
+  
+  var name = document.myform.inputname.value;
+  var lastname = document.myform.inputlastname.value;
+  var x = document.myform.email.value;
+  var contactadd = document.myform.phone.value;
+  var orgname = document.myform.company_name.value;
+  var company_addr = document.myform.orgadd.value;
+
+ 
+  let busid = document.getElementById("bcategory");
+  
+  var checkboxes = busid.querySelectorAll('input[type="checkbox"]:checked');
+  var values = [];
+  for (let i=0; i< checkboxes.length; i++){
+      let business_values = checkboxes[i].value;
+      values.push(business_values)
+      if (business_values == 'others'){
+        working_others()
+      }
+
+  }
+
+  var specific_product = document.myform.specific_product.value;
+  var specific_product1 = document.myform.specific_product1.value;
+  
+  var vdradios = document.getElementsByName('exampleRadios');
+  var selectedradios = '';
+  for (var i = 0; i < vdradios.length; i++) {
+    if (vdradios[i].checked) {
+      selectedradios = vdradios[i].value;
+      break;
+    }
+  }
+
+  // console.log('Selected gender: ' + selectedradios);
+
+  var intfounder = document.getElementsByName('xampleRadios');
+  var selectedintfounder = '';
+  for (var i = 0; i < intfounder.length; i++) {
+    if (intfounder[i].checked) {
+      selectedintfounder = intfounder[i].value;
+      break;
+    }
+  }
+
+  // console.log('Selected gender: ' + selectedintfounder);
+
+
+  let expid = document.getElementById("expanid");
+  
+  var checkboxed = expid.querySelectorAll('input[type="checkbox"]:checked');
+  var exp_values = [];
+  for (let i=0; i< checkboxed.length; i++){
+      let business_exp_values = checkboxed[i].value;
+      exp_values.push(business_exp_values)
+
+  }
+  
+  var subject = document.myform.isubj.value;
+  var your_comment = document.myform.comments.value;
+
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "abfy.website@gmail.com",
+    Password: "D8CEB7F5A3EE48981DBD76FA9EA23CD3DDC9",
+    To: 'honeypriya6741@gmail.com',
+    From: "abfy.website@gmail.com",
+    Subject: subject ,
+    Body: "Name:" + name +  "<br/> last name:" +  lastname +  "<br/> Email:" +  x +  "<br/> Phonenum:" +  contactadd +  "<br/> Name of Organization:" +  orgname +  "<br/> Organization Address:" +  company_addr + "<br/> Business category:" +  values +  "<br/> Specific Product:" +  specific_product1 +  "<br/> Vshoot:" +  selectedradios +  "<br/> InterviewFounder:" +  selectedintfounder +  "<br/> Mode of expansion:" +  exp_values +  "<br/> Comments:" +  your_comment
+  }).then(
+  alert("Sending email successfully....")
+  );
+
+  // console.log("Name:" + name +  "<br/> last name:" +  lastname +  "<br/> Email:" +  email +  "<br/> Phonenum:" +  contactadd +  "<br/> Name of Organization:" +  orgname +  "<br/> Organization Address:" +  company_addr + "<br/> Business category:" +  values +  "<br/> Specific Product:" +  specific_product1 +  "<br/> Vshoot:" +  selectedradios +  "<br/> InterviewFounder:" +  selectedintfounder +  "<br/> Mode of expansion:" +  exp_values +  "<br/> Comments:" +  your_comment );
+
+}
+
+
+
+
+function working_others() {
+  let item_check = document.getElementById("blankCheckbox30");
+    if(item_check.checked) {
+      document.getElementById('select-ev-session').style.display="block"
+    }else{
+      document.getElementById('select-ev-session').style.display="none"
+    }
+   
+  
+}
+
+
+function contmail(){
+  // debugger
+  var fname = document.contform.con_name.value;
+  var y = document.contform.con_email.value;
+  var cnum = document.contform.con_phone.value;
+  var csub = document.contform.con_sub.value;
+  var cmsg = document.contform.con_message.value;
+
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "abfy.website@gmail.com",
+    Password: "D8CEB7F5A3EE48981DBD76FA9EA23CD3DDC9",
+    To: 'honeypriya6741@gmail.com',
+    From: "abfy.website@gmail.com",
+    Subject: csub ,
+    Body: "Name:" + fname +  "<br/> Email:" +  y + "<br/> Phonenum:" +  cnum + "<br/> Message:" +  cmsg
+  }).then(
+  alert("Sending mail successfully....")
+  );
+
+}
+
+  // var name = document.getElementById("name").value;
+  // var y = document.getElementById("email").value;
+  // var Contact = document.getElementById("Phone").value;
+
+//   let message = document.getElementById("message").value;
+//   Email.send({
+//   Host : "smtp.elasticemail.com",
+//   Username : "abfy.website@gmail.com",
+//   Password :"D8CEB7F5A3EE48981DBD76FA9EA23CD3DDC9",
+//   To : 'honeypriya6741@gmail.com',
+//   From : "abfy.website@gmail.com",
+//   Subject : "This is the subject",
+//   Body : "Name:" + name + "<br/> For email:" +  y + "<br/> Subject:" + subject + "<br/> Message:"
+//   + message
+// }).then(
+//   alert("Sending email successfully....")
+// );
+
+// console.log("Name:" + fname +  "<br/> Email:" +  y + "<br/> Phonenum:" +  cnum + "<br/> Message:" +  cmsg );
+  
+
+
+// function Mail() {
+//   email.send({
+//   Subject : subject,
+//   Body :"Name:" + name + "<br/> Form email:" +  email + "<br/> Subject:" + subject + "<br/> Message:"
+//       + message
+    
+// }).then(
+// message => alert("Your message has been sending sucessfully.")
+// );
+// }
+
+
+
+// Email.send({
+//   Host : "smtp.elasticemail.com",
+//   Username : "username",
+//   Password : "password",
+//   To : 'them@website.com',
+//   From : "you@isp.com",
+//   Subject : "This is the subject",
+//   Body : "And this is the body"
+// }).then(
+// message => alert(message)
+// );
+
+
+// checkboxes.length
+
+ // var bid = document.myform.businessid.value;
+  // var bid1 = document.myform.businessid1.value;
+  // var exid = document.myform.expanid.value;
+  // var comment = document.myform.comments.value;
+
+
